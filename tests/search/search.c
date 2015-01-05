b34f1f2a1754a5cee7ad7884385f0ec782dabad4
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-01-05 15:08:06 +0100

    non regression tests for code mutations

---
+++ tests/search/search.c
@@ -0,0 +1,22 @@
+
+/* run.config
+OPT: -main search -mut -mut-msg-key mutant,progress
+*/
+
+/*@ requires \valid(t+(0..4));
+  @*/
+void search(int*t, int x) {
+  int i;
+  int found = 0;
+
+  /*@ loop invariant 0 <= i <= 5;
+    @ loop invariant found <==> (\exists integer k; 0 <= k < i && t[k] == x);
+    @ loop assigns found, i;
+    @*/
+  for(i = 0; i <= 4; i++) {
+    if(t[i] == x)
+      found = 1;
+  }
+
+  //@ assert found <==> \exists integer i; 0 <= i <= 4 && t[i] == x;
+}
